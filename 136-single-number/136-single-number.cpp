d7360ec2b37fe18e4d83f693f7aabe68922a26e4
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-06 21:21:33 -0700

    Time: 22 ms (75.16%), Space: 16.9 MB (53.32%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) 
     {
-        int sum = 0;        for( int i = 0; i< nums.size(); i++ )
+        int sum = 0;        
+        for( int i = 0; i< nums.size(); i++ )
             sum = sum ^ nums[i];
         return sum ;
     }
