dddfa959fcaa29795543169197ab4b9fb6766865
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-06 21:21:13 -0700

    Time: 29 ms (50.35%), Space: 16.9 MB (86.95%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -3,10 +3,9 @@ public:
     int singleNumber(vector<int>& nums) 
     {
         int sum = 0;
+        
         for( int i = 0; i< nums.size(); i++ )
-        {
             sum = sum ^ nums[i];
-        }
         return sum ;
     }
 };
