512a8d7b387a54b5ef3e432b5cceb1def72d8a8f
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-16 15:14:13 -0700

    Time: 12 ms (54.89%), Space: 13.7 MB (63.31%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,27 +2,19 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) 
     {
-     
-        vector<int> vec = {-1, -1};
-        
+        vector<int> vec = {-1, -1}; 
         int lo = 0;
         int hi = nums.size() - 1;
         
-        //first position
         while( lo <= hi )
         {
-            //to avoid overflow not using mid = (  lo + hi  ) / 2
             int mid = lo + (hi - lo) / 2;
             
             if( nums[ mid ] == target )
             {
-                
-                vec[0] = mid; //storing the potential first position
-                //keep searching on left
+                vec[0] = mid; 
                 hi = mid - 1;
             }
-                
-             //Basic Binary search operations
             else if( nums[ mid ] < target )
                 lo = mid + 1;
             
@@ -30,27 +22,19 @@ public:
                 hi = mid - 1;
         }
         
-        //resetting the values of hi and lo
         lo = 0;
         hi = nums.size() - 1;
         
-        
-        //last position
         while( lo <= hi )
         {
-            //to avoid overflow not using mid = (  lo + hi  ) / 2
             int mid = lo + (hi - lo) / 2;
             
             if( nums[ mid ] == target )
-            {
-                
-                vec[1] = mid; //storing the potential last position
-                
-                // keep searching on right
+            {                
+                vec[1] = mid; 
                 lo = mid + 1;
             }
              
-            //Basic Binary search operations
             else if( nums[ mid ] < target )
                 lo = mid + 1;
             
