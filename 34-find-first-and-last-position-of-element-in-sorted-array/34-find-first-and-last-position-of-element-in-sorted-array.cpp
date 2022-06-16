b47818ee456b5f7a0d889afdfb7b449cd3403d92
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-16 15:11:31 -0700

    Time: 12 ms (54.89%), Space: 13.6 MB (63.31%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,29 +3,62 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) 
     {
      
-          int start = 0;
-        int end = nums.size()-1;
+        vector<int> vec = {-1, -1};
         
-        if (nums.size()==1){
-            if (nums[0] == target)
-                return {0,0};
-            return {-1,-1};
+        int lo = 0;
+        int hi = nums.size() - 1;
+        
+        //first position
+        while( lo <= hi )
+        {
+            //to avoid overflow not using mid = (  lo + hi  ) / 2
+            int mid = lo + (hi - lo) / 2;
+            
+            if( nums[ mid ] == target )
+            {
+                
+                vec[0] = mid; //storing the potential first position
+                //keep searching on left
+                hi = mid - 1;
+            }
+                
+             //Basic Binary search operations
+            else if( nums[ mid ] < target )
+                lo = mid + 1;
+            
+            else
+                hi = mid - 1;
         }
         
+        //resetting the values of hi and lo
+        lo = 0;
+        hi = nums.size() - 1;
         
-        while (start < end){
-            if (nums[start] != target){
-                start++;
-            }
-            if (nums[end] != target){
-                end--;
-            }
+        
+        //last position
+        while( lo <= hi )
+        {
+            //to avoid overflow not using mid = (  lo + hi  ) / 2
+            int mid = lo + (hi - lo) / 2;
             
-            if (nums[start] == target && nums[end] == target){
-                return {start, end};
+            if( nums[ mid ] == target )
+            {
+                
+                vec[1] = mid; //storing the potential last position
+                
+                // keep searching on right
+                lo = mid + 1;
             }
+             
+            //Basic Binary search operations
+            else if( nums[ mid ] < target )
+                lo = mid + 1;
             
+            else
+                hi = mid - 1;
         }
-        return {-1, -1};
+        
+        return vec;
+        
     }
 };
