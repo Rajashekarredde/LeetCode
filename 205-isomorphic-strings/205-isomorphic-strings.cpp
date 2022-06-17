19b09db3ac23d60fe3fb7bbd100315ef49bc0a8c
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-17 02:04:17 -0700

    Time: 11 ms (46.20%), Space: 7 MB (46.69%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -8,15 +8,16 @@ public:
      const int sSize = s.size();
     
         for (int i = 0; i < sSize; ++i)   
-        {
-            
+        {   
             if (letters.find(s[i]) == letters.end() && visited[t[i]] == false)
             {
                 letters[s[i]] = t[i];
                 visited[t[i]] = true;
             }
             else if (letters[s[i]] != t[i])
+            {
                 return false;
+            }
         }
     return true;
         
