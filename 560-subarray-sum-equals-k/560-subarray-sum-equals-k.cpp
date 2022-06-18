18cc0f90bbd55cd264cceb0f8398feefd8053513
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-18 13:03:47 -0700

    Time: 99 ms (77.64%), Space: 41.3 MB (68.71%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -3,30 +3,31 @@ class Solution
   public:
     int subarraySum(vector<int>& nums, int k) 
     {
-        int count = 0;
-        int sum = 0;
-        unordered_map< int, int> store;
+        unordered_map< int, int > store;
         
-        for( int i = 0; i<nums.size(); ++i )
+        int n = nums.size(); int sum = 0; int count = 0;
+        
+        for( int i = 0 ; i<n; ++i )
         {
-            sum += nums[i];
+             sum += nums[i];
             
-            if( sum == k ) count ++;
+             if( sum == k ) count++;
             
-            if( store.find( sum - k) != store.end() )
-            {
-                count = store[ sum - k ] + count;
-            }
-            
-            if( store.find( sum ) != store.end() )
-            {
-                store[sum]++;
-            }
-            else
-            {
-                store[sum ] = 1;
-            }
+             if( store.find( sum - k) != store.end() )
+             {
+                 count = store[ sum - k ] + count;
+             }
+                 
+             if( store.find( sum ) == store.end() )
+             {
+                 store[sum] = 1;
+             }
+             else
+             {
+                 store[sum]++;
+             }
         }
-      return count;
+        return count;
+        
     }
 };
