53922d1a16feee93a3c967338bc0ea02fce4cac5
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-18 19:16:44 -0700

    Time: 206 ms (67.25%), Space: 83.1 MB (40.76%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -3,15 +3,12 @@ public:
     int firstMissingPositive(vector<int>& nums) 
     {
         sort( nums.begin(), nums.end());   
-        
         int n = 1;
-        
         for( int i = 0 ; i<nums.size(); ++i )
         {
             if( nums[i] == n )
                 n++;
         }
-        
         return n;
     }
 };
