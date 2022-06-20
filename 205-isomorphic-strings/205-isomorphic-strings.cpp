106f8129ac71b601dd7e7100290b25366af243f2
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-20 13:55:27 -0700

    Time: 10 ms (53.27%), Space: 7 MB (75.14%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) 
     {
-       unordered_map< char, char> store;
-       unordered_map< char, bool> visited; 
-       
-       int m = s.size();
+         unordered_map< char, char> store;
+         unordered_map< char, bool > visited;
+         
+         int m = s.size();
+         int n = t.size();
         
-       int n = t.size();
+         if( m != n ) return false;
         
-        if( m != n ) return false;
-        
-       for( int i = 0; i<m; ++i )
-       {
-          if( store.find( s[i]) == store.end() && visited[ t[i] ] == false )
-          {
-              store[ s[i] ] = t[i];
-              visited[ t[i] ] = true;
-          }
-           
-          if( store[ s[i]] != t[i] ) return false; 
-       }
-       return true;
+         for( int i =0; i< m; ++i )
+         {
+             if( store.find( s[i] ) == store.end() && visited[ t[i] ] == false  )
+             {
+                   store[ s[i] ] = t[i];
+                   visited[t[i]] = true;
+             }
+             
+             if( store[ s[i]] != t[i]  )
+             {
+                 return false;
+             }
+         }
+         return true;
     }
 };
