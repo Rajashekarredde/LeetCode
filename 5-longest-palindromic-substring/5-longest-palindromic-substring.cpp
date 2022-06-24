2c72591da84a9ffd31001103487b580bab0e0d44
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-23 19:58:48 -0700

    Time: 42 ms (72.41%), Space: 7.3 MB (73.36%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,43 +2,43 @@ class Solution {
 public:
     string longestPalindrome(string s) 
     {
-        int l, h;
-        
-        int start =0, end = 1;
+        int l, h, start =0, end = 1;
         int n = s.length();
         
-        for( int i = 1; i< n; ++i )
+        for( int i = 1; i<n; ++i )
         {
             l = i - 1;
-            h = i;
+            h  = i;
             
-            while( l >= 0 && h < n && s[l] == s[h] )
+            while( l >=0 && h < n && s[l] == s[h] )
             {
-                if( h - l + 1 > end  )
+                if( h - l + 1 > end )
                 {
-                    end = h - l + 1;
                     start = l;
+                    end = h - l + 1;
                 }
                 l--; h++;
             }
             
-            
             l = i - 1;
-            h = i + 1;
+            h  = i + 1;
             
-            while( l >= 0 && h < n && s[l] == s[h] )
+            while( l >=0 && h < n && s[l] == s[h] )
             {
-                if( h - l + 1 > end  )
+                if( h - l + 1 > end )
                 {
-                    end = h - l + 1;
                     start = l;
+                    end = h - l + 1;
                 }
                 l--; h++;
             }
         }
         
         string t;
-        for( int i =start; i< (start + end); ++i )
+        
+        n = start + end ;
+        
+        for( int i = start; i< n ; ++i )
         {
             t.push_back( s[i] );
         }
