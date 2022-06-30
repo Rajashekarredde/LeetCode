6816f3623ef749a9c183551c9ca1ac2b2991a268
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-06-30 02:33:04 -0500

    Time: 158 ms (7.89%), Space: 41.1 MB (5.01%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) 
     {
-        map<multiset<char>, vector<string>> charSetToWords;
-       
-        for (auto &str : strs) {
-            const multiset<char> charSet(str.begin(), str.end());
-            charSetToWords[charSet].emplace_back(str);
-        }
+         map< multiset<char>, vector<string> > store;
+         
+         for( int i=0; i<strs.size(); ++i )
+         {
+             multiset<char> charSet( strs[i].begin(), strs[i].end() );
+             store[charSet].emplace_back( strs[i] );
+         }
         
-        vector<vector<string>> results;
-        for (auto &item : charSetToWords) {
-            results.emplace_back(item.second);
-        }
-        return results;
+         vector<vector<string> > ans;
+         
+         for( auto &item : store )
+         {
+             ans.emplace_back( item.second );
+         }
+     return   ans;
     }
 };
