7ffe34af39722f459102023df011c1621a5f331f
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-07-05 01:40:43 -0500

    Time: 15 ms (85.04%), Space: 14.6 MB (27.05%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -3,27 +3,28 @@ public:
     bool divideArray(vector<int>& nums) 
     {
         unordered_map< int, int> store;
+        int count = 0;
         int n = nums.size();
-        int pair = n/ 2;
-
+        
         for( int i = 0; i<n; ++i )
         {
-           if( nums[i] == INT_MIN ) continue;
-            
-           for( int j = i + 1; j<n; ++j )
-           {
-               if( nums[i] == nums[j] )
-               {
-                   nums[i] = INT_MIN;
-                   nums[j] = INT_MIN;
-                   pair --;
-                   break;
-               }
-           }
+            if( store.find( nums[i] ) == store.end() )
+                store[ nums[i] ] = 1;
+            else
+                store[ nums[i] ]++;
         }
         
-        if( !pair ) return true;
-        else return false;
+        for( auto itr = store.begin(); itr != store.end(); ++itr )
+        {
+            if( itr->second%2 == 0 )
+            {
+                count += itr->second/2;
+            }
+        }
         
+        if( ( nums.size()/2 ) == count )
+            return true ;
+        else 
+                return false;
     }
 };
