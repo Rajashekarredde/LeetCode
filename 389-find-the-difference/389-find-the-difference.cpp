542b51b7b2686a223a6788a7f093096c0023702f
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-07-07 16:29:31 -0700

    Time: 6 ms (41.92%), Space: 6.9 MB (9.35%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,26 +2,12 @@ class Solution {
 public:
     char findTheDifference(string s, string t) 
     {
-        int n = t.length();
-        int m = s.length();
-        bool isFound;
-        
-        for( int i=0; i<n; ++i )
+        s += t; 
+        int c =0;
+        for( auto k : s)
         {
-            isFound = false;
-            for( int j =0; j<m; ++j )
-            {
-                if( t[i]  == s[j] )
-                {
-                    isFound = true;
-                    s[j] = '\0';
-                    break;
-                }
-            }
-            
-            if( !isFound )
-                return t[i];
+            c^=k;
         }
-        return ' ';
+        return c;
     }
 };
