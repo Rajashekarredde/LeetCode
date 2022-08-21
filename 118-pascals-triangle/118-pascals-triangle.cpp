95aa971052be950222fd720969cfe17832b75660
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-08-21 10:35:47 -0700

    Time: 2 ms (46.31%), Space: 6.5 MB (32.76%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,21 +3,21 @@ public:
     vector<vector<int>> generate(int n) 
     {
         vector< vector<int> > ans;
-        
-        for( int i=0; i<n; ++i )
+        for( int i = 0; i<n; ++i )
         {
-            vector<int> temp(i+1);
+            vector<int >temp(i+1);
             temp[0] = 1;
             temp[i] = 1;
-            if( i == 0 || i == 1 )
+            if( i  == 0 || i == 1 )
             {
-                ans.push_back( temp );
+                ans.push_back(temp);
                 continue;
             }
             
-            for( int j = 1; j<i; j++ )
-                temp[j] = ans[i-1][j-1] + ans [i-1][j]; 
-            
+            for( int j = 1; j<i; ++j )
+            {
+                temp[j] = ans[i-1][j-1] + ans[i-1][j];
+            }
             ans.push_back( temp );
         }
         return ans;
