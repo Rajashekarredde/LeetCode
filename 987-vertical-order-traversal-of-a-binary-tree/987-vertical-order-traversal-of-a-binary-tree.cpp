69bce14b3fb6bab26f8d53e3bf0fea90d1ff964f
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-08-28 18:45:18 -0700

    Time: 3 ms (93.90%), Space: 13.7 MB (20.50%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,36 +13,39 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) 
     {
-        map< int, map< int, multiset<int> >> mp;
-        queue< pair< TreeNode*, pair<int, int> >>q;
+        queue< pair< TreeNode*, pair<int, int> > > q;
+        map< int, map<int, multiset<int> > > mp;
         q.push( {root, {0, 0}} );
         while( !q.empty() )
         {
             auto it = q.front();
             q.pop();
             TreeNode *node = it.first;
-            int x = it.second.first; int y = it.second.second;
-            mp[x][y].insert(node->val);
+            
+            int x = it.second.first;
+            int y = it.second.second;
+            mp[x][y].insert( node->val );
             if( node->left )
             {
-                q.push({node->left, {x-1, y+1}});
+                q.push( {node->left, {x-1, y+1}} );
             }
             if( node->right )
             {
-                q.push({node->right, {x+1, y+1}});
+                q.push( {node->right, {x+1, y+1}} );
             }
         }
         
-        vector< vector<int> >ans;
-        for( auto it : mp )
+        vector< vector<int >> ans;
+        
+        for( auto p : mp )
         {
             vector<int> temp;
-            for( auto p : it.second )
+            for( auto q : p.second )
             {
-                temp.insert( temp.end(), p.second.begin(), p.second.end() );
+                temp.insert( temp.end(), q.second.begin(), q.second.end() );
             }
-            ans.push_back( temp );
+            ans.push_back( temp) ;
         }
-        return ans;
+        return ans ;
     }
 };
