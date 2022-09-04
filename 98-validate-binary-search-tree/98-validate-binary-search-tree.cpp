376879ab4fb667fd435b78484e78bc534615bea5
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-09-03 22:23:41 -0700

    Time: 21 ms (43.96%), Space: 21.9 MB (15.64%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,21 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class info
+{
+    public: 
+    long long int maxi;
+    long long int  mini;
+    bool isValid;
+};
+
+info solve( TreeNode *root )
+{
+    if( !root )
+        return {LONG_MIN, LONG_MAX, true };
+    
+    auto left = solve( root->left);
+    auto right = solve( root->right );
+    
+    info cur;
+    cur.maxi = max<long long int>( right.maxi, root->val );
+    cur.mini = min<long long int>( left.mini, root->val );
+    cur.isValid = false;
+    
+    if( left.isValid && right.isValid && ( left.maxi < root->val && root->val < right.mini ) )
+    {
+        cur.isValid= true;
+    }
+    return cur;
+}
+
 class Solution {
 public:
     bool isValidBST(TreeNode* root) 
     {   
-        return traversal( root, LONG_MIN, LONG_MAX );
-    }
-    
-    bool traversal( TreeNode *root, long left, long right )
-    {
-        if( !root ) return true;
-
-        if(  root->val >= right || root->val <= left )
-            return false;
-         
-        return ( traversal( root->left, left, root->val ) and
-                 traversal( root->right, root->val, right ));
-    }
+        if( !root )
+            return true;
+        return solve( root).isValid;
+    }  
 };
