6324cdc0694fcb2a166a40993c022b5194f77074
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-09-04 17:35:28 -0700

    Time: 237 ms (38.72%), Space: 73 MB (28.87%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string t) 
+    int longestPalindromeSubseq(string s) 
     {
-      string s = t;
-      reverse( t.begin(), t.end() );
-      vector< vector<int> > dp(s.length(), vector<int>(t.length(), -1) );
-      return solve( s, t, 0, 0, dp );
+        string t = s;
+        reverse( t.begin(), t.end() );
+        vector< vector<int> >dp( s.length(), vector<int>( t.length(), -1 ) );
+        return solve( s, t, 0, 0, dp );
     }
     
-    int solve( string &s, string &t, int i, int j, vector< vector<int> >&dp )
+    int solve( string &s, string &t, int i, int j, 
+                    vector< vector<int> >&dp )
     {
-        if( i == s.length() || j == t.length() )
+        if( i == s.length() )
             return 0;
-        
+        if( j == t.length() )
+            return 0;
         if( dp[i][j] != -1 )
             return dp[i][j];
+        int ans= 0;
         
-        int ans =0;
         if( s[i] == t[j] )
-        {
-            ans = 1 + solve( s, t, i+1, j +1, dp );
-        }
+            ans = 1+ solve( s, t, i+1, j+1, dp );
         else
-        {
-            ans = max( solve(s,t,i, j+1, dp ), solve( s, t, i+1, j, dp) );
-        }
+            ans = max( solve( s, t, i+1, j, dp ), solve( s, t, i, j+1, dp ) );
         
-        return dp[i][j]=ans;
+        return dp[i][j] = ans;
     }
+
 };
