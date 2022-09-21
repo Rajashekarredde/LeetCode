412b2ada94b8c03d2a0d94b59285787d59dfdf25
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-09-20 21:33:34 -0700

    Time: 41 ms (83.51%), Space: 12.3 MB (55.06%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,34 +1,34 @@
 class Solution {
-public:
-    void solver( int i, int j, int r, int c, vector< vector<char> >&grid )
+public: 
+    void solve( vector< vector<char> >&grid, int i, int j, int row, int col )
     {
-        if( i < 0 || i >=r || j < 0 || j >= c || grid[i][j] != '1' )
-            return;
+        if( i < 0 || i >= row || j < 0 || j >= col || grid[i][j] != '1' )
+            return ;
+        
         grid[i][j] = '2';
         
-        int d_row[] = {1, 0, -1, 0 };
+        int d_row[] = {1, 0, -1, 0};
         int d_col[] = {0, 1, 0, -1};
-        for( int k = 0; k<4; k++ )
-        {
-            solver( i+d_row[k], j+d_col[k], r, c, grid );
-        }
+        
+        for( int k = 0; k < 4; k++ )
+            solve( grid, d_row[k] + i, d_col[k]+j, row, col );
     }
     
     int numIslands(vector<vector<char>>& grid) 
     {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<int>q;
         int count = 0;
         
-        for( int i = 0; i<n; ++i )
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for( int i = 0; i<n; i++ )
         {
-            for( int j = 0; j<m; ++j )
+            for( int j = 0; j<m; j++ )
             {
                 if( grid[i][j] == '1' )
                 {
-                   solver(i,j, n, m, grid );
-                   count++;
+                    count++;
+                    solve( grid, i, j, n, m );
                 }
             }
         }
