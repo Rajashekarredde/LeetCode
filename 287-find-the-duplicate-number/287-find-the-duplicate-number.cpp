f3eacc431c444f560e72815ea7d832b427fa2666
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-09-24 14:12:40 -0700

    Time: 109 ms (98.19%), Space: 61.3 MB (62.08%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -4,11 +4,11 @@ public:
     {
        for( int i = 0; i<nums.size(); ++i )
        {
-           if( nums[ abs(nums[i]) ] < 0 )
+           if( nums[ abs(nums[i]) - 1 ] < 0 )
            {
                return abs( nums[i] );
            }
-           nums[ abs(nums[i]) ] *= -1 ;
+           nums[ abs(nums[i]) - 1 ] *= -1 ;
        }
        return 0;
     }
